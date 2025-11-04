8bd2e44b2dccbf105827123a8fdebe33e5701761
Malik-Rizwan125 <99021298+Malik-Rizwan125@users.noreply.github.com>
2025-11-04 22:36:34 +0500

    logo marquee code update

--- src/app/components/portfolio/LogoMarquee.tsx
+++ src/app/components/portfolio/LogoMarquee.tsx
@@ -4,40 +4,36 @@ import Image from "next/image";
 import React from "react";
 
 const logos = [
-  "/images/logo/logo1.png",
-  "/images/logo/logo2.png",
-  "/images/logo/logo3.png",
-  "/images/logo/logo4.jpg",
-  "/images/logo/logo5.jpg",
-  "/images/logo/logo6.png",
-  "/images/logo/logo7.png",
-  // "/images/logo/logo8.png",
-  "/images/logo/logo9.png",
-  "/images/logo/logo10.png",
-  // "/images/logo/logo11.png",
-  // "/images/logo/logo12.png",
-  // "/images/logo/logo13.png",
-  // "/images/logo/logo14.png",
+  { src: "/images/logo/logo1.png", contain: false },
+  { src: "/images/logo/logo2.png", contain: false },
+  { src: "/images/logo/logo3.png", contain: true },
+  { src: "/images/logo/logo4.jpg", contain: false },
+  { src: "/images/logo/logo5.jpg", contain: false },
+  { src: "/images/logo/logo6.png", contain: false },
+  { src: "/images/logo/logo7.png", contain: false },
+  { src: "/images/logo/logo9.png", contain: true },
+  { src: "/images/logo/logo10.png", contain: true },
 ];
 
 export default function LogoMarquee() {
   return (
-    <section className="w-full overflow-hidden ">
+    <section className="w-full overflow-hidden">
       <div className="relative flex overflow-hidden h-[200px]">
         {/* Marquee Container */}
         <div className="flex animate-marquee whitespace-nowrap">
-          {/* Duplicate logos for seamless infinite scroll */}
           {[...logos, ...logos].map((logo, index) => (
             <div
               key={index}
               className="flex-shrink-0 flex justify-center items-center px-10"
             >
               <Image
-                src={logo}
+                src={logo.src}
                 alt={`Logo ${index + 1}`}
-                width={100}
-                height={100}
-                className="h-[100px] w-[100px] opacity-80 hover:opacity-100 transition-all duration-300 object-cover rounded-full"
+                width={150}
+                height={150}
+                className={`h-[150px] w-[150px] border border-white opacity-80 hover:opacity-100 transition-all duration-300 p-1 rounded-full ${
+                  logo.contain ? "object-contain bg-white" : "object-cover"
+                }`}
               />
             </div>
           ))}
